9de5e3e30caf63afec4b376d359fa40a03c5819f
mehmet20251990 <mehmet20251990@gmail.com>
2023-04-17 22:32:41 +0300

    update

--- src/main/java/utils/ElementHelper.java
+++ src/main/java/utils/ElementHelper.java
@@ -1,36 +1,29 @@
 package utils;
 
-import io.appium.java_client.TouchAction;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import java.time.Duration;
 import java.util.List;
-
 public class ElementHelper {
-
     private WebDriver driver;
     private WebDriverWait wait;
-
     public ElementHelper(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
-
-
+    public WebElement findElement(By locator) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
     public void checkElement(By locator) {
-        driver.findElement(locator);
+        findElement(locator);
     }
-
     public void clickElement(By locator) {
-        WebElement element = driver.findElement(locator);
-        element.click();
+        findElement(locator).click();
     }
-
     public void clickListElement(By locator, String text) {
         boolean check = false;
         List<WebElement> elements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
@@ -43,16 +36,10 @@ public class ElementHelper {
         }
         Assert.assertTrue(check);
     }
-
     public void sendKeys(By locator, String text) {
-        WebElement element = driver.findElement(locator);
-        element.sendKeys(text);
+        findElement(locator).sendKeys(text);
     }
-
-
     public void scroll(By locator, String text) {
 
     }
-
-
 }
