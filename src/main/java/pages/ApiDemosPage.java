329864b3d94b060e89c8964a1b27505433ecb440
mehmet20251990 <mehmet20251990@gmail.com>
2023-04-18 21:51:30 +0300

    update

---
+++ src/main/java/pages/ApiDemosPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.ElementHelper;
+public class ApiDemosPage {
+    ElementHelper elementHelper;
+    By actionBar = By.id("android:id/action_bar");
+    By title = By.xpath("//*[@resource-id=\"android:id/action_bar\"]/android.widget.TextView");
+    By tab_App = By.xpath("//android.widget.TextView[@content-desc=\"App\"]");
+    By tab_actionBar = By.xpath("//android.widget.TextView[@content-desc=\"Action Bar\"]");
+    By tab_actionBarUsage = By.xpath("//android.widget.TextView[@content-desc=\"Action Bar Usage\"]");
+    By tabs_list = By.id("android:id/text1");
+    public ApiDemosPage(WebDriver driver) {
+        this.elementHelper = new ElementHelper(driver);
+    }
+    public void checkApiDemosPageOpen() {
+        elementHelper.checkElement(actionBar);
+    }
+    public String getApiDemosTitle() {
+        return elementHelper.getText(title);
+    }
+    public void clickAppTab() {
+        elementHelper.clickElement(tab_App);
+    }
+    public void clickActionBarTab() {
+        elementHelper.clickElement(tab_actionBar);
+    }
+    public void clickActionBarUsageTab() {
+        elementHelper.clickElement(tab_actionBarUsage);
+    }
+    public void clickTab(String text) {
+        elementHelper.clickElementWithText(tabs_list,text);
+    }
