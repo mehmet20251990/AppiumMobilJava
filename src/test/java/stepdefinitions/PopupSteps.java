ad45dcd778fb7cf76b16ec2e9a1d017d9bed0bf6
mehmet20251990 <mehmet20251990@gmail.com>
2023-04-19 22:32:13 +0300

    update

---
+++ src/test/java/stepdefinitions/PopupSteps.java
@@ -0,0 +1,11 @@
+package stepdefinitions;
+import io.cucumber.java.en.When;
+import pages.PopupPage;
+import utils.DriverManager;
+public class PopupSteps {
+    PopupPage popupPage = new PopupPage(DriverManager.getDriver());
+    @When("taps {string} button on Popup")
+    public void tapsButtonOnPopup(String button) {
+        popupPage.clickButton(button);
+    }
+}
