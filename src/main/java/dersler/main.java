1ab6c32ddf50f074ef6b3798522abfd12258b59c
mehmet20251990 <mehmet20251990@gmail.com>
2023-04-25 21:11:53 +0300

    update

--- src/main/java/dersler/main.java
+++ src/main/java/dersler/main.java
@@ -18,6 +18,21 @@ public class main {
     5-) Page sınıfındaki metodların içine kullanacakları elementHelper metodları tanımlanıcak
     6-) Casei çalıştırıp hataları kontrol edip düzelticem
      */
+    /*
+    1- Scenario yu yazmak (Given-When-Then)
+    2- Scenario daki steplerin metodlar ile bağlantısı için step definition dosyalarını oluşturmak.
+    3- Step definition dosyalarının içini doldurmak
+    4- Page sınıflarını oluşturdum
+    5- Stepdefinitionlardaki nesne üretme adımlarını kendi pagelerine göre ayarladım.
+    6- Step definition metodlarımın içindeki değişkenlerin isimlerini düzelttim
+    7- Step definition cümlelerimin içinde page sınıfındaki metodları çağırdım ama bu metodlar daha page sınıfında
+       yazılı değil.
+    8- Page sınıfında, step definitionda oluşturduğum metod isimleriyle metodlar oluşturdum.
+    9- Metodların içini gerekli elementHelper metodlarıyla doldurucam.
+    10- Elementleri ekle
+    11- Testini koş hataları düzelt. Noktalı virgül, featureda verdiğin dinamik texti yanlış yazmış olabilirsin,
+        elementi yanlış almış olabilirsin.
+     */
     public static void main(String[] args) throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
