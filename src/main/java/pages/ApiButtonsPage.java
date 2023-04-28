ae3672de7a85dc475bec56a2630fa5a206582d5a
mehmet20251990 <mehmet20251990@gmail.com>
2023-04-28 21:01:22 +0300

    update

--- src/main/java/pages/ApiButtonsPage.java
+++ src/main/java/pages/ApiButtonsPage.java
@@ -1,12 +1,36 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.ElementHelper;
-
 public class ApiButtonsPage {
     ElementHelper elementHelper;
     public ApiButtonsPage(WebDriver driver) {
         this.elementHelper = new ElementHelper(driver);
     }
-
+    By title = By.className("android.widget.TextView");
+    By normalButton = By.id("io.appium.android.apis:id/button_normal");
+    By smallButton = By.id("io.appium.android.apis:id/button_small");
+    By switchButton = By.id("io.appium.android.apis:id/button_toggle");
+    public String getTitle() {
+        return elementHelper.getText(title);
+    }
+    public void checkButonsPage() {
+        elementHelper.checkElement(title);
+    }
+    public void checkNormalButton() {
+        elementHelper.checkElement(normalButton);
+    }
+    public String getTextNormalButton() {
+        return elementHelper.getText(normalButton);
+    }
+    public String getTextSmallButton() {
+        return elementHelper.getText(smallButton);
+    }
+    public String getTextSwitchButton() {
+        return elementHelper.getText(switchButton);
+    }
+    public void clickSwitchButton() {
+        elementHelper.click(switchButton);
+    }
 }
--- src/main/java/pages/ApiCategoriesPage.java
+++ src/main/java/pages/ApiCategoriesPage.java
@@ -1,12 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.ElementHelper;
-
 public class ApiCategoriesPage {
     ElementHelper elementHelper;
     public ApiCategoriesPage(WebDriver driver) {
         this.elementHelper = new ElementHelper(driver);
     }
-
+    By title = By.id("android:id/action_bar");
+    By categories = By.id("android:id/text1");
+    public void checkCategoriesPage() {
+        elementHelper.checkElement(title);
+    }
+    public void clickCategory(String category) {
+        elementHelper.clickElementWithText(categories,category);
+    }
 }
